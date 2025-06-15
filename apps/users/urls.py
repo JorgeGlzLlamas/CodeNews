f4ab96cd5c7164284f80df97dffebb09731dfa5a
JorgeGlzLamas <jorgeglzllamas.dev@gmail.com>
2025-06-14 21:27:42 -0700

    Update users urls.py
    Añadir las rutas para registro, inicio sesion y cerrar sesion

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,18 @@
-# from django.urls import path
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+from users.views import users
 
 app_name = 'users'
 
 urlpatterns = [
-
+    path('registro/',
+         users.RegistrationView.as_view(),
+         name='register'),
+    path('inicio-sesion/',
+         users.AuthenticationView.as_view(),
+         name='login'),
+    path('cerrar-sesion/',
+         LogoutView.as_view(next_page='core:home'),
+         name='logout')
 ]
