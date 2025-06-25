bfb4e6365fbc83d860276937d5990a49919c6f6f
JorgeGlzLamas <jorgeglzllamas.dev@gmail.com>
2025-06-25 16:10:33 -0700

    build: version-inicial

--- apps/users/views/users.py
+++ apps/users/views/users.py
@@ -4,11 +4,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import AuthenticationForm
 from users.forms.user_form import UserRegistrationForm
 from django.contrib import messages
-<<<<<<< HEAD
 from django.contrib.auth.views import LogoutView
-=======
-
->>>>>>> parent of e0311f4 (Autenticacion con google)
 from users.models.user import User
 from users.models.user_rol import Rol
 
@@ -27,14 +23,10 @@ class RegistrationView(CreateView):
         return super().form_valid(form)
 
     def form_invalid(self, form):
-<<<<<<< HEAD
         messages.success(
             self.request,
             '¡Tu cuenta ya estaba registrada!.'
         )
-=======
-        messages.error(self.request, 'Hubo un error al registrarte.')
->>>>>>> parent of e0311f4 (Autenticacion con google)
         return super().form_invalid(form)
 
     def get_success_url(self):
@@ -43,18 +35,16 @@ class RegistrationView(CreateView):
 
 class AuthenticationView(LoginView):
     """Authentication view for users."""
-
     authentication_form = AuthenticationForm
     template_name = 'authentication.html'
 
     def form_valid(self, form):
-        user = form.get_user()
-        messages.success(self.request, f'¡Bienvenido {user.username} a CodeNews!')
-        return super().form_valid(form)
+        response = super().form_valid(form)
+        messages.success(self.request, '¡Has iniciado sesión correctamente!')
+        return response
 
     def get_success_url(self):
         return reverse('core:home')
-<<<<<<< HEAD
 
 
 class CustomLogoutView(LogoutView):
@@ -63,5 +53,3 @@ class CustomLogoutView(LogoutView):
         if 'user_id' in request.session:
             del request.session['user_id']
         return super().dispatch(request, *args, **kwargs)
-=======
->>>>>>> parent of e0311f4 (Autenticacion con google)
