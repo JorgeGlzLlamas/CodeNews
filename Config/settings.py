d47f5cf05651f754fdcd3dc3ac9d4680ed6bf837
JorgeGlzLamas <jorgeglzllamas.dev@gmail.com>
2025-06-26 16:16:56 -0700

    Update database

--- Config/settings.py
+++ Config/settings.py
@@ -101,15 +101,10 @@ WSGI_APPLICATION = 'Config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-   "default": {
-       # "ENGINE": "django.db.backends.postgresql",
-       "ENGINE": "django.db.backends.postgresql_psycopg2",
-       "NAME": env.str("DB_NAME"),
-       "USER": env.str("DB_USER"),
-       "PASSWORD": env.str("DB_PWD"),
-       "HOST": env.str("DB_HOST"),
-       "PORT": env.str("DB_PORT"),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
