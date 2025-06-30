482ecdee42d61961d118a109cf038c34e15d686f
Junior Renteria <106351834+Jrenteria19@users.noreply.github.com>
2025-06-30 16:44:19 -0700

    Perfil de usuario actual
    Perfil de usuario actual

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
-
+from django.contrib.auth.views import LogoutView, PasswordChangeView
 from users.views import users
 
 app_name = 'users'
 
 urlpatterns = [
+    # Authentication URLs
     path('registro/',
          users.RegistrationView.as_view(),
          name='register'),
@@ -14,5 +14,19 @@ urlpatterns = [
          name='login'),
     path('cerrar-sesion/',
          LogoutView.as_view(next_page='core:home'),
-         name='logout')
+         name='logout'),
+    
+    # Profile URLs
+    path('perfil/',
+         users.ProfileView.as_view(),
+         name='profile'),
+    path('perfil/actualizar/',
+         users.ProfileUpdateView.as_view(),
+         name='update_profile'),
+    path('perfil/avatar/actualizar/',
+         users.AvatarUpdateView.as_view(),
+         name='update_avatar'),
+    path('perfil/avatar/eliminar/',
+         users.AvatarUpdateView.as_view(),
+         name='delete_avatar'),
 ]
