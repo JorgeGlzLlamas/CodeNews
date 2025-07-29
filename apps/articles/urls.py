f4be09f7ae508324a9a18b50596c6e11435a2d8a
JorgeGlzLamas <jorgeglzllamas.dev@gmail.com>
2025-07-28 23:40:47 -0700

    add: paths for articles

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -1,7 +1,21 @@
-# from django.urls import path
+from django.urls import path
+from articles.views import articles
 
 app_name = 'articles'
 
 urlpatterns = [
-
+    # Articles URLs
+    path('nuevo/',
+         articles.ArticleCreateView.as_view(),
+         name='article_create'),
+    path('<slug:title>/contenido/',
+         articles.ArticleContentView.as_view(),
+         name='article_content'),
+    path('<slug:title>/editar/',
+         articles.ArticleUpdateView.as_view(),
+         name='article_update'),
+    # Set update flag for redirecting after content creation
+    path('<slug:slug>/set-update-flag/',
+         articles.set_from_update_view,
+         name='set_update_flag')
 ]
