360cab055749868b1d9ace9d0eaecb467d9bb2c5
JorgeGlzLlamas <jorgeglzllamas.dev@gmail.com>
2025-08-01 01:11:32 -0700

    Add SocialMediaForm

--- apps/users/forms/perfil_usuario.py
+++ apps/users/forms/perfil_usuario.py
@@ -2,6 +2,7 @@ from django import forms
 
 from users.models.user import User
 from users.models.user_privacity import UserPrivacy
+from users.models.socialmedia import SocialMedia
 
 
 class UserProfileForm(forms.ModelForm):
@@ -49,8 +50,8 @@ class UserProfilePrivacyForm(forms.ModelForm):
     class Meta:
         model = UserPrivacy
         fields = [
-            'username_or_name', 'bio_is_public', 
-            'email_is_public','phone_is_public',   
+            'username_or_name', 'bio_is_public',
+            'email_is_public', 'phone_is_public',
         ]
         widgets = {
             'username_or_name': forms.Select(attrs={
@@ -65,4 +66,48 @@ class UserProfilePrivacyForm(forms.ModelForm):
             'phone_is_public': forms.CheckboxInput(attrs={
                 'class': 'form-check-input'
             }),
+        }
+
+
+class UserProfileSocialMediaForm(forms.ModelForm):
+    """
+    Formulario para gestionar las redes sociales del usuario.
+    Filtra las plataformas para excluir las que el usuario ya ha añadido.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Recibir el usuario desde la vista"""
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        """Asegurarnos de que tenemos un usuario"""
+        if not self.user:
+            return
+
+        """Filtrar las opciones del campo 'platform'"""
+        existing_platforms_qs = SocialMedia.objects.filter(user=self.user)
+        if self.instance and self.instance.pk:
+            existing_platforms_qs = existing_platforms_qs.exclude(pk=self.instance.pk)        
+        existing_platforms = list(existing_platforms_qs.values_list('platform', flat=True))
+        all_choices = SocialMedia.PlaformChoices.choices
+        
+        # Creamos una nueva lista de opciones, incluyendo solo aquellas
+        # cuyo valor NO ESTÁ en la lista de plataformas existentes.
+        available_choices = [
+            (value, label) for value, label in all_choices if value not in existing_platforms
+        ]
+        self.fields['platform'].choices = available_choices
+
+    class Meta:
+        model = SocialMedia
+        fields = [
+            'platform', 'link'
+        ]
+        widgets = {
+            'platform': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+            'link': forms.URLInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'https://...'
+            })
         }
